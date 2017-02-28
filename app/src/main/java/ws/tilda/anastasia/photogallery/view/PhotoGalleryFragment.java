76071746ded09b9bca270c89584c6f7a9b18d762
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2017-02-28 15:33:31 +0200

    writing an AsyncTask, created the background thread and ran some networking code on it

--- app/src/main/java/ws/tilda/anastasia/photogallery/view/PhotoGalleryFragment.java
+++ app/src/main/java/ws/tilda/anastasia/photogallery/view/PhotoGalleryFragment.java
@@ -1,18 +1,25 @@
 package ws.tilda.anastasia.photogallery.view;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.io.IOException;
+
 import ws.tilda.anastasia.photogallery.R;
+import ws.tilda.anastasia.photogallery.model.FlikrFetchr;
 
 
 public class PhotoGalleryFragment extends Fragment {
+
+    public static final String TAG = "PhotoGalleryFragment";
 
     private RecyclerView mPhotoRecyclerView;
 
@@ -24,6 +31,7 @@ public class PhotoGalleryFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
+        new FetchItemsTask().execute();
     }
 
     @Nullable
@@ -37,4 +45,22 @@ public class PhotoGalleryFragment extends Fragment {
 
         return v;
     }
+
+    private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try{
+                String result = new FlikrFetchr()
+                        .getUrlString("https://anastasia.tilda.ws");
+                Log.i(TAG, "Fetched contents of URL: " + result);
+            } catch (IOException ioe) {
+                Log.e(TAG, "Failed to fetch URL: ", ioe);
+            }
+
+            return null;
+        }
+    }
+
+
 }
