09c64cfc4d1243f1443e4e09356e2273a86637b8
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2017-03-01 11:57:13 +0200

    parsing Flikr photos and calling this method in fetchItems()

--- app/src/main/java/ws/tilda/anastasia/photogallery/view/PhotoGalleryFragment.java
+++ app/src/main/java/ws/tilda/anastasia/photogallery/view/PhotoGalleryFragment.java
@@ -6,12 +6,9 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import java.io.IOException;
 
 import ws.tilda.anastasia.photogallery.R;
 import ws.tilda.anastasia.photogallery.controller.FlikrFetchr;
